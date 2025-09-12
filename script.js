4bec9e16fcfa95d72704b3afed4710b6ec88cbc1
faruk <farukislamfarabi35@gmail.com>
2025-09-13 03:12:19 +0600

    md file added

--- script.js
+++ script.js
@@ -17,20 +17,20 @@ window.onload = function () {
         categories.forEach(data => {
             let singleBtn = document.createElement('div')
             singleBtn.innerHTML = `
-                <button class=" category-buttons w-[90%] rounded text-start my-2 pl-2"> ${data.category_name} </button>
+                <button class=" category-buttons w-[90%] hover:bg-green-300 rounded text-start my-2 pl-2 py-2"> ${data.category_name} </button>
             `;
             buttonCategories.appendChild(singleBtn)
         })
         let singleBtn = document.createElement('div')
-        singleBtn.innerHTML = `<button id="allTrees" class=" category-buttons w-[90%] rounded text-start my-2 pl-2"> All Trees </button>`;
+        singleBtn.innerHTML = `<button id="allTrees" class="category-buttons hover:bg-green-300 bg-green-600 w-[90%] rounded text-start my-2 pl-2 py-2"> All Trees </button>`;
         buttonCategories.prepend(singleBtn)
         allTrees();
         
         let categoryButtons = document.querySelectorAll('.category-buttons')
         categoryButtons.forEach((data, index) => {
             data.addEventListener('click', () => {
-                categoryButtons.forEach(data => data.classList.remove('bg-green-500'))
-                data.classList.add('bg-green-500')
+                categoryButtons.forEach(data => data.classList.remove('bg-green-600'))
+                data.classList.add('bg-green-600')
                 let url = `https://openapi.programming-hero.com/api/category/${index}`
                 Secondlayout.innerHTML = '';
                 fetch(url)
@@ -49,9 +49,20 @@ window.onload = function () {
 function allTrees () {
     let allTrees = document.getElementById('allTrees')
     allTrees.addEventListener('click', () => {
+        
+        let categoryButtons = document.querySelectorAll('.category-buttons');
+        categoryButtons.forEach(btn => btn.classList.remove('bg-green-600'));
+        allTrees.classList.add('bg-green-600');
+        
     Secondlayout.innerHTML = '';
-    buttonCategories.innerHTML = '';
-    window.onload();
+    fetch('https://openapi.programming-hero.com/api/plants')
+    .then(res => res.json())
+    .then(data => {
+        let plants = data.plants;
+        plants.forEach(function (data) {
+            reUsuableCard(data);
+        })
+    })
 })}
 
 let cart = [];
@@ -104,16 +115,16 @@ function reUsuableCard (data) {
             div.innerHTML = `
                     <div class="bg-white rounded w-[240px]">
                         <div class="my-card p-2">
-                            <img class="w-[220px] h-[300px] rounded" src="${data.image}" alt="">
+                            <img class="w-[220px] h-[250px] rounded" src="${data.image}" alt="">
                             <h2 class="title text-xl font-bold mt-2"> ${data.name}</h2>
-                            <p class="mb-2 opacity-75 text-sm py-2">${data.description}</p>
+                            <p class="mb-2 opacity-75 text-sm pb-5 pt-2 truncate-text">${data.description}</p>
                             <div class="flex justify-between">
                                 <h3 class="tag bg-[#dcfce7] rounded-lg px-4">${data.category}</h3>
                                 <div class="taka">
                                     <p>${data.price} </p>
                                 </div>
                             </div>
-                            <button class="addToCart w-full bg-green-500 rounded-lg mt-3">Add to cart</button>
+                            <button class="addToCart w-full bg-green-600 rounded-lg mt-3 hover:text-white">Add to cart</button>
                         </div>
                     </div>
             `;
